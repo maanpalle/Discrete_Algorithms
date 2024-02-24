76cc8cea7f24f6ba0bd4d30076ad15a1627c8a8a
1Kobe2 <kobebleuze@live.be>
2024-02-24 17:01:12 +0100

    restructure BranchAndBound

--- project1/src/main/java/be/ugent/BranchAndBound.java
+++ project1/src/main/java/be/ugent/BranchAndBound.java
@@ -1,23 +1,14 @@
 package be.ugent;
 
-
 import be.ugent.graphs.BasicGraph;
 
 import java.util.BitSet;
 
 public class BranchAndBound implements MaximumCliqueAlgorithm {
-    public static void main(String[] args) {
-        long start = System.currentTimeMillis();
-        BasicGraph graph = new BasicGraph("DIMACS_subset_ascii/C125.9.clq");
-        BitSet maxClique = MaxClique(graph);
-        System.out.println(maxClique);
-        System.out.println(graph.isClique(maxClique));
-        System.out.println(maxClique.cardinality());
-        long end = System.currentTimeMillis();
-        System.out.println(end - start);
-    }
+    private BitSet biggestClique;
+    private BasicGraph graph;
 
-    public static BitSet MaxClique(BitSet clique, BitSet remaining, BitSet biggestClique, BasicGraph graph) {
+    private BitSet MaxClique(BitSet clique, BitSet remaining) {
         int id = remaining.nextSetBit(0);
         while (id != -1 && clique.cardinality() + remaining.cardinality() > biggestClique.cardinality()) {
             clique.set(id);
@@ -26,7 +17,7 @@ public class BranchAndBound implements MaximumCliqueAlgorithm {
             newRemaining.and(graph.getAdjacencyBitSet(id));
             BitSet maxClique = clique;
             if (newRemaining.cardinality() != 0) {
-                maxClique = MaxClique(clique, newRemaining, biggestClique, graph);
+                maxClique = MaxClique(clique, newRemaining);
             }
             if (maxClique.cardinality() > biggestClique.cardinality()) {
                 biggestClique = (BitSet) maxClique.clone();
@@ -37,15 +28,25 @@ public class BranchAndBound implements MaximumCliqueAlgorithm {
         return biggestClique;
     }
 
-    public static BitSet MaxClique(BasicGraph graph) {
-        int numVertices = graph.getNumVertices();
-        BitSet allVertices = new BitSet(numVertices);
-        allVertices.set(0, numVertices);
-        return MaxClique(new BitSet(numVertices), allVertices, new BitSet(numVertices), graph);
-    }
-
     @Override
     public BitSet calculateMaxClique(BasicGraph graph) {
-        return null;
+        this.graph = graph;
+        int numVertices = graph.getNumVertices();
+        biggestClique = new BitSet(numVertices);
+        BitSet allVertices = new BitSet(numVertices);
+        allVertices.set(0, numVertices);
+        return MaxClique(new BitSet(numVertices), allVertices);
+    }
+
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        BranchAndBound bAB = new BranchAndBound();
+        BasicGraph graph = new BasicGraph("DIMACS_subset_ascii/C125.9.clq");
+        BitSet maxClique = bAB.calculateMaxClique(graph);
+        System.out.println(maxClique);
+        System.out.println(maxClique.cardinality());
+        System.out.println(graph.isClique(maxClique));
+        long end = System.currentTimeMillis();
+        System.out.println(end - start);
     }
 }
