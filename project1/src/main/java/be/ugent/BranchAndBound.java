c00b90b5adb01737d1ba562bd770ec020d7be1d7
1Kobe2 <kobebleuze@live.be>
2024-02-27 04:45:38 +0100

    BranchAndBound add greedy lower bound + cleanup

--- project1/src/main/java/be/ugent/BranchAndBound.java
+++ project1/src/main/java/be/ugent/BranchAndBound.java
@@ -10,23 +10,22 @@ public class BranchAndBound implements MaximumCliqueAlgorithm {
     private BitSet biggestClique;
     private BasicGraph gr;
 
-    private BitSet MaxClique(BitSet clique, BitSet remaining, List<BitSet> colors) {
+    private BitSet MaxClique(BitSet clique, BitSet remaining) {
         int id = remaining.nextSetBit(0);
-        while (id != -1 && clique.cardinality() + countColors(remaining, colors) > biggestClique.cardinality()) {
+        while (id != -1 && clique.cardinality() + greedyColor(remaining) > biggestClique.cardinality()) {
             clique.set(id);
             remaining.clear(id);
             BitSet newRemaining = (BitSet) remaining.clone();
             newRemaining.and(gr.getAdjacencyBitSet(id));
             BitSet maxClique = clique;
             if (newRemaining.cardinality() != 0) {
-                maxClique = MaxClique(clique, newRemaining, greedyColor(newRemaining));
+                maxClique = MaxClique(clique, newRemaining);
             }
             if (maxClique.cardinality() > biggestClique.cardinality()) {
                 biggestClique = (BitSet) maxClique.clone();
             }
             clique.clear(id);
-            id = remaining.nextSetBit(id);
-            colors = greedyColor(remaining);
+            id = remaining.nextSetBit(id + 1);
         }
         return biggestClique;
     }
@@ -37,16 +36,10 @@ public class BranchAndBound implements MaximumCliqueAlgorithm {
         List<Integer> vertices = gr.orderByUpwardsDegree();
         gr.reorderVertices(vertices);
         int numVertices = vertices.size();
-        float minSize = 0;
-        for (int i = 0; i < numVertices; i++) {
-            minSize += (1.0f / (numVertices - gr.degree(i)));
-        }
-        biggestClique = new BitSet(numVertices);
-        biggestClique.set(0, (int) minSize);
+        greedyClique();
         BitSet allVertices = new BitSet(numVertices);
         allVertices.set(0, numVertices);
-        List<BitSet> colors = greedyColor(allVertices);
-        BitSet maxClique = MaxClique(new BitSet(numVertices), allVertices, colors);
+        BitSet maxClique = MaxClique(new BitSet(numVertices), allVertices);
         BitSet solution = new BitSet(numVertices);
         int id = maxClique.nextSetBit(0);
         while (id != -1) {
@@ -56,7 +49,7 @@ public class BranchAndBound implements MaximumCliqueAlgorithm {
         return solution;
     }
 
-    private List<BitSet> greedyColor(BitSet vert) {
+    private int greedyColor(BitSet vert) {
         List<BitSet> colors = new ArrayList<>();
         int id = vert.previousSetBit(gr.getNumVertices());
         while (id != -1) {
@@ -70,20 +63,22 @@ public class BranchAndBound implements MaximumCliqueAlgorithm {
             colors.get(color).set(id);
             id = vert.previousSetBit(id - 1);
         }
-        return colors;
+        return colors.size();
     }
 
-    private int countColors(BitSet vertices, List<BitSet> colors) {
-        int count = 0;
-        for (BitSet color : colors) {
-            if (color.intersects(vertices)) {
-                count++;
-            }
+    private void greedyClique() {
+        BitSet remaining = new BitSet(gr.getNumVertices());
+        remaining.set(0, gr.getNumVertices());
+        biggestClique = new BitSet(gr.getNumVertices());
+        int id = remaining.previousSetBit(gr.getNumVertices() + 1);
+        while (id != -1) {
+            biggestClique.set(id);
+            remaining.and(gr.getAdjacencyBitSet(id));
+            id = remaining.previousSetBit(id - 1);
         }
-        return count;
     }
 
-    private void test(BasicGraph graph){
+    private void test(BasicGraph graph) {
         BitSet maxClique = calculateMaxClique(graph);
         System.out.println(maxClique);
         System.out.println(maxClique.cardinality());
